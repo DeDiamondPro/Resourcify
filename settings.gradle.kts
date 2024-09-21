0a1dcd8c6a0fd979993b3db0feb8310174990112
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2024-09-21 17:50:00 +0200

    Update to loom 1.6.17 again

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ pluginManagement {
         maven("https://repo.polyfrost.org/releases")
         mavenLocal()
     }
-    val egtVersion = "0.6.2"
+    val egtVersion = "0.6.2+diamond.loom1.6.17"
     plugins {
         id("gg.essential.multi-version.root") version egtVersion
     }
