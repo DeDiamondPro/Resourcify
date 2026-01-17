965f616f3aaa57385a5fccde8d50cf498b1f0c97
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2026-01-17 21:35:48 +0100

    Add GIF support to markdown

--- src/main/kotlin/dev/dediamondpro/resourcify/elements/markdown/ResourcifyMarkdownImageElement.kt
+++ src/main/kotlin/dev/dediamondpro/resourcify/elements/markdown/ResourcifyMarkdownImageElement.kt
@@ -1,6 +1,6 @@
 /*
  * This file is part of Resourcify
- * Copyright (C) 2025 DeDiamondPro
+ * Copyright (C) 2025-2026 DeDiamondPro
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,41 +19,29 @@ package dev.dediamondpro.resourcify.elements.markdown
 
 import dev.dediamondpro.minemark.LayoutStyle
 import dev.dediamondpro.minemark.elementa.style.MarkdownStyle
-import dev.dediamondpro.minemark.elementa.util.EmptyImage
 import dev.dediamondpro.minemark.elements.Element
 import dev.dediamondpro.minemark.elements.impl.ImageElement
-import gg.essential.elementa.components.UIImage
+import dev.dediamondpro.resourcify.elements.image.IUIImage
 import gg.essential.universal.UMatrixStack
 import org.xml.sax.Attributes
 import java.awt.Color
-import java.awt.image.BufferedImage
-import java.util.concurrent.CompletableFuture
 
 
-// Custom markdown image element so we can draw images using linear interpolation instead of nearest
-// TODO: this should be merged into MineMark at some point
 class ResourcifyMarkdownImageElement(
     style: MarkdownStyle,
     layoutStyle: LayoutStyle,
     parent: Element<MarkdownStyle, UMatrixStack>?,
     qName: String, attributes: Attributes?
-) : ImageElement<MarkdownStyle, UMatrixStack, BufferedImage>(style, layoutStyle, parent, qName, attributes) {
-    private var uiImage: UIImage? = null
-
+) : ImageElement<MarkdownStyle, UMatrixStack, IUIImage>(style, layoutStyle, parent, qName, attributes) {
     override fun drawImage(
-        image: BufferedImage,
+        image: IUIImage,
         x: Float,
         y: Float,
         width: Float,
         height: Float,
         matrixStack: UMatrixStack
     ) {
-        if (uiImage == null) {
-            uiImage = UIImage(CompletableFuture.supplyAsync { image }, EmptyImage)
-            uiImage?.textureMinFilter = UIImage.TextureScalingMode.LINEAR
-            uiImage?.textureMagFilter = UIImage.TextureScalingMode.LINEAR
-        }
-        uiImage?.drawImage(
+        image.drawImage(
             matrixStack,
             x.toDouble(),
             y.toDouble(),
--- src/main/kotlin/dev/dediamondpro/resourcify/util/SanitizingImageProvider.kt
+++ src/main/kotlin/dev/dediamondpro/resourcify/util/SanitizingImageProvider.kt
@@ -1,6 +1,6 @@
 /*
  * This file is part of Resourcify
- * Copyright (C) 2024 DeDiamondPro
+ * Copyright (C) 2024-2026 DeDiamondPro
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,14 +17,57 @@
 
 package dev.dediamondpro.resourcify.util
 
-import dev.dediamondpro.minemark.providers.DefaultImageProvider
-import java.io.InputStream
-import java.net.URI
+import dev.dediamondpro.minemark.providers.ImageProvider
+import dev.dediamondpro.resourcify.elements.image.IUIImage
+import dev.dediamondpro.resourcify.elements.image.UIAnimatedImage
+import dev.dediamondpro.resourcify.elements.image.UIImageWrapper
+import gg.essential.elementa.components.UIImage
+import java.util.concurrent.CompletableFuture
+import java.util.function.Consumer
+import javax.imageio.ImageIO
 
-object SanitizingImageProvider : DefaultImageProvider() {
-    override fun getInputStream(src: String): InputStream? {
-        return ImageURLUtils.getTransformedImageUrl(src.toURI()).toURL().setupConnection().apply {
-            setRequestProperty("Accept", "image/*")
-        }.getEncodedInputStream()
+object SanitizingImageProvider : ImageProvider<IUIImage> {
+    override fun getImage(
+        src: String,
+        dimensionCallback: Consumer<ImageProvider.Dimension>,
+        imageCallback: Consumer<IUIImage>
+    ) {
+        CompletableFuture.runAsync {
+            val transformedUri = ImageURLUtils.getTransformedImageUrl(src.toURI())
+            (transformedUri.toURL().setupConnection().apply {
+                setRequestProperty("Accept", "image/*")
+            }.getEncodedInputStream() ?: error("Failed to load image $src")).use {
+                if (UIAnimatedImage.supportsExtension(ImageURLUtils.getExtension(transformedUri))) {
+                    val image = UIAnimatedImage.fromGif(it, loadAsync = false, loadingImage = EmptyImage)
+                    dimensionCallback.accept(ImageProvider.Dimension(image.imageWidth, image.imageHeight))
+                    imageCallback.accept(image)
+                } else {
+                    ImageIO.createImageInputStream(it).use { imageIn ->
+                        val readers = ImageIO.getImageReaders(imageIn)
+                        check(readers.hasNext()) { "No image reader found for $src" }
+                        val reader = readers.next()
+                        try {
+                            reader.setInput(imageIn)
+                            dimensionCallback.accept(
+                                ImageProvider.Dimension(
+                                    reader.getWidth(0).toFloat(),
+                                    reader.getHeight(0).toFloat()
+                                )
+                            )
+                            imageCallback.accept(
+                                UIImageWrapper(
+                                    UIImage(
+                                        supply { reader.read(0) },
+                                        EmptyImage
+                                    )
+                                )
+                            )
+                        } finally {
+                            reader.dispose()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
